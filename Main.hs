39a069430a0bb7097f8dfc2fc08a6854b7db9bd6
Sam Elliott <sam@lenary.co.uk>
2014-11-13 20:48:19 +0000

    Initial Breakout into seperate repo

---
+++ Main.hs
@@ -0,0 +1,41 @@
+module Main where
+
+import Idris.Core.TT
+import Idris.AbsSyntax
+import Idris.ElabDecls
+import Idris.REPL
+
+import IRTS.Compiler
+import IRTS.CodegenErlang
+
+import System.Environment
+import System.Exit
+
+import Paths_idris
+
+data Opts = Opts { inputs :: [FilePath],
+                   output :: FilePath }
+
+showUsage = do putStrLn "Usage: idris-erlang <ibc-files> [-o <output-file>]"
+               exitWith ExitSuccess
+
+getOpts :: IO Opts
+getOpts = do xs <- getArgs
+             return $ process (Opts [] "idris.erl") xs
+  where
+    process opts ("-o":o:xs) = process (opts {output = o}) xs
+    process opts (x:xs)      = process (opts {inputs = x:inputs opts}) xs
+    process opts []          = opts
+
+erl_main :: Opts -> Idris ()
+erl_main opts = do elabPrims
+                   loadInputs (inputs opts) Nothing
+                   mainProg <- elabMain
+                   ir <- compile (Via "erlang") (output opts) mainProg
+                   runIO $ codegenErlang ir
+
+main :: IO ()
+main = do opts <- getOpts
+          if (null (inputs opts))
+            then showUsage
+            else runMain (erl_main opts)
